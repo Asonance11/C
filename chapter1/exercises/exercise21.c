ce9aa52864995785c28905baa0b2a16732c7a17f
Oluwadabira <adigunsamuelaso@gmail.com>
2024-05-29 16:47:15 +0100

    Some exercises

---
+++ chapter1/exercises/exercise21.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define TABSTOP 8
+
+int main() {
+  int spaces, c;
+
+  while ((c = getchar()) != EOF) {
+    if (c == ' ') {
+      spaces = TABSTOP;
+      while (spaces) {
+        putchar(' ');
+        spaces--;
+      }
+    } else {
+      putchar(c);
+    }
+  }
+
+  return 0;
+}
