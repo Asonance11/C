7ac02f852955c8b0c8ab4cc70a72435a791f51f5
Oluwadabira <adigunsamuelaso@gmail.com>
2024-05-31 12:24:27 +0100

    Reading and writing binary files

--- fileIO.c
+++ fileIO.c
@@ -34,14 +34,33 @@ int main(void) {
 
   // Writing files
 
-  int x = 32;
+  // int x = 32;
+  //
+  // fp = stdout;
+  //
+  // fputc('S', fp);
+  // fputc('\n', fp);
+  // fprintf(fp, "x = %d \n", x);
+  // fputs("Hello World \n", fp);
 
-  fp = stdout;
+  // binary files
+  // Writing
 
-  fputc('S', fp);
-  fputc('\n', fp);
-  fprintf(fp, "x = %d \n", x);
-  fputs("Hello World \n", fp);
+  // unsigned char bytes[6] = {2, 4, 6, 7, 8, 8};
+  //
+  // fp = fopen("output.bin", "wb");
+  //
+  // fwrite(bytes, sizeof(char), 6, fp);
+
+  // Reading
+
+  unsigned char c;
+
+  fp = fopen("output.bin", "rb");
+
+  while (fread(&c, sizeof(char), 1, fp) > 0) {
+    printf("%d \n", c);
+  }
 
   fclose(fp);
 }
