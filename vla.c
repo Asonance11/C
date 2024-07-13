327ada0345569b3ed9f36f66ed6e8f8f4c4a8fd3
Oluwadabira <adigunsamuelaso@gmail.com>
2024-07-13 20:35:20 +0100

    VLA with multi arrays

--- vla.c
+++ vla.c
@@ -2,12 +2,22 @@
 #include <stdlib.h>
 
 // Variable length arrays
+//
+// Multi dimensional VLAs
+void print_matrix(int h, int w, int m[h][w]) {
+  for (int row = 0; row < h; row++) {
+    for (int col = 0; col < w; col++) {
+      printf("%2d ", m[row][col]);
+    }
+    printf("\n");
+  }
+}
 
 int main(void) {
   int n;
   char buf[32];
 
-  printf("Enter a number");
+  printf("Enter a number: ");
   fflush(stdout);
   fgets(buf, sizeof buf, stdin);
   n = strtoul(buf, NULL, 10);
@@ -19,4 +29,17 @@ int main(void) {
 
   for (int i = 0; i < n; i++)
     printf("v[%d] = %d\n", i, v[i]);
+
+  // multi
+  int rows = 5;
+  int cols = 6;
+
+  int matrix[rows][cols];
+
+  for (int row = 0; row < rows; row++) {
+    for (int col = 0; col < cols; col++)
+      matrix[row][col] = row * col;
+  }
+
+  print_matrix(rows, cols, matrix);
 }
